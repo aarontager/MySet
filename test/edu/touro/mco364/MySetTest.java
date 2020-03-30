cb80f2db8627905cce4e6b98ee2dfd15277c8735
aarontager <tageraharon@gmail.com>
2020-03-30 16:49:15 -0400

    Started project, implementing methods

---
+++ test/edu/touro/mco364/MySetTest.java
@@ -0,0 +1,60 @@
+package edu.touro.mco364;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MySetTest {
+
+    @Test
+    void size() {
+    }
+
+    @Test
+    void isEmpty() {
+    }
+
+    @Test
+    void contains() {
+    }
+
+    @Test
+    void iterator() {
+    }
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void testToArray() {
+    }
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void remove() {
+    }
+
+    @Test
+    void containsAll() {
+    }
+
+    @Test
+    void addAll() {
+    }
+
+    @Test
+    void retainAll() {
+    }
+
+    @Test
+    void removeAll() {
+    }
+
+    @Test
+    void clear() {
+    }
+}
