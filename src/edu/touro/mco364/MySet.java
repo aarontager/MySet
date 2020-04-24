4c1082c4ad1a1f135d94450b6f30848b0563c22e
aarontager <tageraharon@gmail.com>
2020-04-24 11:39:39 -0400

    Everything besides last test

--- src/edu/touro/mco364/MySet.java
+++ src/edu/touro/mco364/MySet.java
@@ -1,17 +1,20 @@
 package edu.touro.mco364;
 
+import javafx.beans.binding.ObjectExpression;
+
+import java.io.IOException;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
 
 public class MySet<T> implements Serializable, Set<T> {
-    T[] backingStore = (T[]) new Object[10];
-    int insertionPoint = 0;
+    private T[] backingStore = (T[]) new Object[10];
+    private int insertionPoint = 0;
 
     @Override
     public int size() {
-        return insertionPoint + 1;
+        return insertionPoint;
     }
 
     @Override
@@ -21,8 +24,8 @@ public class MySet<T> implements Serializable, Set<T> {
 
     @Override
     public boolean contains(Object o) {
-        for (int i = 0; i < insertionPoint; i++) {
-            if (o.equals(backingStore[i])) {
+        for(T t: backingStore) {
+            if (o.equals(t)) {
                 return true;
             }
         }
@@ -31,17 +34,40 @@ public class MySet<T> implements Serializable, Set<T> {
 
     @Override
     public Iterator<T> iterator() {
-        return null;
+        return new MySetIterator();
+    }
+
+    private class MySetIterator implements Iterator<T> {
+        private int index = 0;
+
+        @Override
+        public boolean hasNext() {
+            return index < size();
+        }
+
+        @Override
+        public T next() {
+            return backingStore[index++];
+        }
     }
 
     @Override
     public Object[] toArray() {
-        return new Object[0];
+        T[] returnArray = (T[]) new Object[size()];
+        System.arraycopy(backingStore, 0, returnArray, 0, size());
+        return returnArray;
     }
 
     @Override
     public <T1> T1[] toArray(T1[] t1s) {
-        return null;
+        if(t1s.length < size()) {   //Doesn't fit
+            t1s = (T1[]) new Object[size()];
+        }
+        else if(t1s.length > size()) {
+            t1s[size()] = null;
+        }
+        System.arraycopy(backingStore, 0, t1s, 0, size());
+        return t1s;
     }
 
     @Override
@@ -56,33 +82,79 @@ public class MySet<T> implements Serializable, Set<T> {
 
     @Override
     public boolean remove(Object o) {
+        for (int i = 0; i < size(); i++) {
+            if(backingStore[i].equals(o)) {
+                System.arraycopy(backingStore, i + 1, backingStore, i, size() - i);
+                insertionPoint--;
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean containsAll(Collection<?> collection) {
-        return false;
+        for(Object element : collection) {
+            if(!contains(element)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean addAll(Collection<? extends T> collection) {
-        return false;
+        boolean changed = false;
+
+        for(T element : collection) {
+            if(add(element)) {
+                changed = true;
+            }
+        }
+        return changed;
     }
 
     @Override
     public boolean retainAll(Collection<?> collection) {
-        return false;
+        T[] newBS = (T[]) new Object[insertionPoint];
+        int tempIP = 0;
+
+        for (int i = 0; i < insertionPoint; i++) {
+            if(collection.contains(backingStore[i])) {
+                newBS[tempIP++] = backingStore[i];
+            }
+        }
+        backingStore = newBS;
+        insertionPoint = tempIP;
+        return insertionPoint == backingStore.length - 1;
     }
 
     @Override
     public boolean removeAll(Collection<?> collection) {
-        return false;
+        boolean changed = false;
+
+        for(Object element : collection) {
+            if(remove(element)) {
+                changed = true;
+            }
+        }
+        return changed;
     }
 
     @Override
     public void clear() {
         backingStore = (T[]) new Object[10];
         insertionPoint = 0;
+    }
+
+    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+        out.writeObject(backingStore);
+        out.writeObject(insertionPoint);
+    }
+
+    private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException {
+        backingStore = (T[]) in.readObject();
+        insertionPoint = in.readInt();
     }
 
     private void ensureCapacity() {
--- test/edu/touro/mco364/MySetTest.java
+++ test/edu/touro/mco364/MySetTest.java
@@ -2,59 +2,107 @@ package edu.touro.mco364;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySetTest {
+    MySet<String> mySet = new MySet<String>();
+    Collection<String> coll = new ArrayList<String>();
 
     @Test
-    void size() {
+    void size() {   //also tests add()
+        assertEquals(0, mySet.size());
+        mySet.add("A");
+        assertEquals(1, mySet.size());
     }
 
     @Test
     void isEmpty() {
+        assertTrue(mySet.isEmpty());
+        mySet.add("A");
+        assertTrue(!mySet.isEmpty());
     }
 
     @Test
     void contains() {
-    }
-
-    @Test
-    void iterator() {
-    }
-
-    @Test
-    void toArray() {
-    }
-
-    @Test
-    void testToArray() {
-    }
-
-    @Test
-    void add() {
+        assertTrue(!mySet.contains("A"));
+        mySet.add("A");
+        assertTrue(mySet.contains("A"));
+        assertTrue(!mySet.contains("a"));
     }
 
     @Test
     void remove() {
+        mySet.add("A");
+        mySet.add("B");
+        mySet.remove("B");
+        assertTrue(!mySet.contains("B"));
+        assertEquals(1, mySet.size());
     }
 
     @Test
     void containsAll() {
+        mySet.add("A");
+        mySet.add("B");
+        mySet.add("C");
+
+        coll.add("A");
+        coll.add("B");
+
+        assertTrue(mySet.containsAll(coll));
+
+        coll.add("D");
+
+        assertTrue(!mySet.containsAll(coll));
     }
 
     @Test
     void addAll() {
+        coll.add("A");
+        coll.add("B");
+
+        mySet.addAll(coll);
+
+        assertTrue(mySet.containsAll(coll));
+        assertEquals(2, mySet.size());
     }
 
     @Test
     void retainAll() {
+        mySet.add("A");
+        mySet.add("B");
+        mySet.add("C");
+
+        coll.add("A");
+        coll.add("B");
+
+        assertTrue(mySet.retainAll(coll));
+        assertEquals(2, mySet.size());
     }
 
     @Test
     void removeAll() {
+        mySet.add("A");
+        mySet.add("B");
+        mySet.add("C");
+
+        coll.add("A");
+        coll.add("B");
+
+        assertTrue(mySet.removeAll(coll));
+        assertEquals(1, mySet.size());
     }
 
     @Test
     void clear() {
+        mySet.add("A");
+        mySet.add("B");
+        mySet.add("C");
+        mySet.clear();
+
+        assertTrue(mySet.isEmpty());
     }
 }
